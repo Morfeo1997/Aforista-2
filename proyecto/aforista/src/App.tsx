401605fe0eebf73016e3c940138d718959ee7841
Morfeo <gastongomez2014@hotmail.com>
2026-05-15 18:52:41 -0300

    dependencias agregadas

---
+++ proyecto/aforista/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="bg-red-500">
+      Hola mundo!
+      </p>
+    </>
+  )
+}
+
+export default App
