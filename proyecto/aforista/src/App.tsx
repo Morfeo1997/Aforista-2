fa493d18ec5f02e4fb0d88e959d8d7f02683b41f
Morfeo <gastongomez2014@hotmail.com>
2026-05-19 16:34:25 -0300

    primera versión funcional

--- proyecto/aforista/src/App.tsx
+++ proyecto/aforista/src/App.tsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import Start from './components/start'
+import Main from './components/main'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [started, setStarted] = useState(false)
 
   return (
     <>
-    	<div>
-    	 	<Start/>
-    	</div>
+    	{!started ? (
+        <Start onComplete={() => setStarted(true)} />
+      ) : (
+        <Main />
+      )}
     </>
   )
 }
