af013ddf201b032e05654f2614a2ebd319de8bd8
Morfeo <gastongomez2014@hotmail.com>
2026-05-22 00:42:06 -0300

    arreglo de bug menor

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import { useState } from 'react'
 import Start from './components/start'
 import Main from './components/main'
+import Header from './components/header'
 
 function App() {
-  const [started, setStarted] = useState(false)
+  const [started, setStarted] = useState(false);
+
+  const [language, setLanguage] = useState("es");
 
   return (
     <>
+    
+      <Header
+        language={language}
+        onLanguageChange={setLanguage}
+      />
     	{!started ? (
         <Start onComplete={() => setStarted(true)} />
       ) : (
-        <Main />
+          <Main />
       )}
     </>
   )
