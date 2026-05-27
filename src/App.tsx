92dde06423483c70214c9cba969e204438733e71
Morfeo <gastongomez2014@hotmail.com>
2026-05-26 23:46:56 -0300

    se agrego la conexion con contexto

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,42 @@
-import { useState } from 'react'
-import Start from './components/start'
-import Main from './components/main'
-import Header from './components/header'
+import { useState } from "react";
 
-function App() {
+import Start from "./components/start";
+import Main from "./components/main";
+import Header from "./components/header";
+
+import { LanguageProvider } from "./context/LanguageContext";
+import { ThemeProvider } from "./context/ThemeContext";
+
+export default function App() {
   const [started, setStarted] = useState(false);
-
   const [language, setLanguage] = useState("es");
 
   return (
-    <>
-    
-      <Header
-        language={language}
+    <LanguageProvider>
+      <ThemeProvider>
+        <div
+          className="min-h-screen transition-colors duration-500"
+          style={{
+            background: "var(--main-gradient)",
+            color: "var(--primary-text)",
+          }}
+        >
+          {/* Header */}
+          <Header 
+          language={language}
         onLanguageChange={setLanguage}
-      />
-    	{!started ? (
-        <Start onComplete={() => setStarted(true)} />
-      ) : (
-          <Main />
-      )}
-    </>
-  )
-}
+        />
 
-export default App
+          {/* Contenido principal */}
+          {!started ? (
+            <Start
+              onComplete={() => setStarted(true)}
+            />
+          ) : (
+            <Main />
+          )}
+        </div>
+      </ThemeProvider>
+    </LanguageProvider>
+  );
+}
